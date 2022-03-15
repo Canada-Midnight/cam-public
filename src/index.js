facc804e37b8d973c9efe048c143970e2e7c64e4
SongeDaiDai <michael.savard@canadaatmidnight.com>
2022-03-15 18:02:20 -0400

    a

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ Coded by www.creative-tim.com
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleLogin } from "react-google-login";
 import App from "App";
 
 ReactDOM.render(
@@ -28,14 +27,3 @@ ReactDOM.render(
 const responseGoogle = (response) => {
   console.log(response);
 };
-
-ReactDOM.render(
-  <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={"single_host_origin"}
-  />,
-  document.getElementById("googleButton")
-);
